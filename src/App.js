4b7d5a411529a348be647fd978ee7d78a88bb98f
simoneas02 <simoneas02@hotmail.com>
2017-05-20 15:21:04 -0300

    versão estática da aplicação criada clode  #23

--- src/App.js
+++ src/App.js
@@ -4,12 +4,66 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <h2>Welcome to React love</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header></header>
+
+          <main>
+            <h1>Contratos</h1>
+
+            <div>
+              <input type="search" placeholder="Localizar contrato"/>
+              <button >Pesquisar</button>
+            </div>
+
+             <div>
+              <select>
+                <option value="codigo">Código</option>
+                <option value="data-acordo">Data acordo comercial</option>
+                <option value="natureza">Natureza</option>
+                <option value="comprador">Comprador</option>
+                <option value="vendedor">Vendedor</option>
+              </select>
+              <button >+</button>
+            </div>
+
+            <table>
+              <thead>
+                <tr>
+                  <th><button>></button>Código<button>x</button></th>
+                  <th><button>></button>Comprador<button>x</button></th>
+                  <th><button>></button>Vendedor<button>x</button></th>
+                  <th><button>></button>Status aprovação<button>x</button></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>123/2012</td>
+                  <td>Capibaribe Energia</td>
+                  <td>Contoso Comercializadora</td>
+                  <td>Aprovado</td>
+                </tr>
+                <tr>
+                  <td>123/2012</td>
+                  <td>Capibaribe Energia</td>
+                  <td>Contoso Comercializadora</td>
+                  <td>Aprovado</td>
+                </tr>
+                <tr>
+                  <td>123/2012</td>
+                  <td>Capibaribe Energia</td>
+                  <td>Contoso Comercializadora</td>
+                  <td>Aprovado</td>
+                </tr>
+                <tr>
+                  <td>123/2012</td>
+                  <td>Capibaribe Energia</td>
+                  <td>Contoso Comercializadora</td>
+                  <td>Aprovado</td>
+                </tr>
+              </tbody>
+            </table>
+          </main>
+
+          <footer></footer>
       </div>
     );
   }
