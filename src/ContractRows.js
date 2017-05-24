9068fb2001ca97a744ce38b65c534c810d8cdfae
simoneas02 <simoneas02@hotmail.com>
2017-05-24 15:59:09 -0300

    dados do contrato listados quando selecionado close #9

--- src/ContractRows.js
+++ src/ContractRows.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class ContractRows extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        contract: []
+      };
+  };
+
+  showContract(contractNumber) {
+    const contract = this.props.list.filter((contract) => {
+      return (contractNumber === contract.codigo);
+    });
+    this.setState({ contract: contract[0] });
+  };
 
   render() {
     const contractsList = this.props.list.map((contract)=> {
@@ -17,12 +30,31 @@ class ContractRows extends Component {
       });
 
       return (
-        <tr key = { contract.codigo }>{ columnsList }</tr>
+        <tr key = { contract.codigo } onClick={ this.showContract.bind(this, contract.codigo) }>{ columnsList }</tr>
         )
     });
 
     return(
-      <tbody>{ contractsList }</tbody>
+      <tbody>
+        { contractsList }
+        <div className="contractDialog">
+          Código: { this.state.contract.codigo}<br/>
+          Data acordo comercial: { this.state.contract.dataacordocomercial}<br/>
+          Natureza: { this.state.contract.natureza}<br/>
+          Comprador: { this.state.contract.comprador}<br/>
+          Vendedor: { this.state.contract.vendedor}<br/>
+          Energia referência: { this.state.contract.energiareferencia}<br/>
+          Energia entregue: { this.state.contract.energiaentregue}<br/>
+          Status aprovação: { this.state.contract.statusaprovacao}<br/>
+          Modelo: { this.state.contract.modelo}<br/>
+          Preço base contratado: { this.state.contract.precobasecontratado}<br/>
+          Submercado: { this.state.contract.submercado}<br/>
+          Início fornecimento: { this.state.contract.iniciofornecimento}<br/>
+          Fim fornecimento: { this.state.contract.fimfornecimento}<br/>
+          Início vigência: { this.state.contract.iniciovigencia}<br/>
+          Fim vigência: { this.state.contract.fimvigencia}<br/>
+        </div>
+      </tbody>
     );
   }
 }
